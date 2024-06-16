5f869ba09cc7c77d650ad9c44707f1564b711db4
Eldiyar <eldiyarbakirov00@gmail.com>
2024-06-16 22:29:46 +0600

    hello new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from '@/components/hero/Hero'
 import Reaception from "@/components/reception/Reception";
 
 export default function Home() {
   return (
     <main>
+      <Hero/>
       <Reaception />
     </main>
   );
