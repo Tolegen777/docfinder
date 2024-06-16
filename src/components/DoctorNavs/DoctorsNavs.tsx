f0842da37c9c73fe15c22b693bd75dd81f86073f
aziret <kubaaziret68@gmail.com>
2024-06-16 17:00:47 +0600

    vbeivew

--- src/components/DoctorNavs/DoctorsNavs.tsx
+++ src/components/DoctorNavs/DoctorsNavs.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const DoctorsNavs: React.FC = () => {
-	return (
-		<div>
-			<section id='doctorNavs'>
-				<div className='container'>
-					<div className='doctorNavs'>
-						<div className='doctorMainNavs'>
-							<button>Скидки %</button>
-							<button>Cмена</button>
-							<button>Детский</button>
-							<h3>Сбросить</h3>
-						</div>
-						<div className='doctorMainSort'></div>
-					</div>
-				</div>
-			</section>
-		</div>
-	)
-}
+  return (
+    <div>
+      <section id="doctorNavs">
+        <div className="container">
+          <div className="doctorNavs">
+            <div className="doctorMainNavs">
+              <button>Скидки %</button>
+              <button>Cмена</button>
+              <button>Детский</button>
+              <h3>Сбросить</h3>
+            </div>
+            <div className="doctorMainSort"></div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default DoctorsNavs
+export default DoctorsNavs;
