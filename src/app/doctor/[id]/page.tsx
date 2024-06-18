7af5b46be8828aa7c760b853e1c62c3710485e09
aziret <kubaaziret68@gmail.com>
2024-06-18 06:45:27 +0600

    .

--- src/app/doctor/[id]/page.tsx
+++ src/app/doctor/[id]/page.tsx
@@ -1,38 +1,38 @@
-  "use client";
-  import DoctorInformation from "@/components/DoctorInformation/DoctorInformation";
-  import DoctorModal from "@/components/DoctorModal/DoctorModal";
-  import Specializations from "@/components/Specializations/Specializations";
-  import React, { useState } from "react";
+"use client";
+import DoctorInformation from "@/components/DoctorInformation/DoctorInformation";
+import DoctorModal from "@/components/DoctorModal/DoctorModal";
+import Specializations from "@/components/Specializations/Specializations";
+import React, { useState } from "react";
 
-  const Page: React.FC = () => {
-    const [modal, setModal] = useState<boolean>(false);
+const Page: React.FC = () => {
+  const [modal, setModal] = useState<boolean>(false);
 
-    const modalFunction = (): void => {
-      setModal((prevModal) => !prevModal);
-    };
-
-    return (
-      <div>
-        <div
-          style={{
-            background: "white",
-            width: "100%",
-          }}
-        >
-          <div>
-            <DoctorInformation modalFunction={modalFunction} />
-          </div>
-          <Specializations />
-        </div>
-        <div
-          style={{
-            display: modal ? "block" : "none",
-          }}
-        >
-          <DoctorModal setModal={modalFunction} />
-        </div>
-      </div>
-    );
+  const modalFunction = (): void => {
+    setModal((prevModal) => !prevModal);
   };
 
-  export default Page;
+  return (
+    <div>
+      <div
+        style={{
+          background: "white",
+          width: "100%",
+        }}
+      >
+        <div>
+          <DoctorInformation modalFunction={modalFunction} />
+        </div>
+        <Specializations />
+      </div>
+      <div
+        style={{
+          display: modal ? "block" : "none",
+        }}
+      >
+        <DoctorModal setModal={modalFunction} />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
