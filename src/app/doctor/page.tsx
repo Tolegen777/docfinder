915dadf3807d7485a372b359ee17f39ef7ec11d4
Eldiyar <eldiyarbakirov00@gmail.com>
2024-06-18 18:25:59 +0600

    end

--- src/app/doctor/page.tsx
+++ src/app/doctor/page.tsx
@@ -1,16 +1,17 @@
 import DoctorInformation from '@/components/DoctorInformation/DoctorInformation'
-import DoctorsNavs from "@/components/DoctorNavs/DoctorsNavs";
+import DoctorsNavs from '@/components/DoctorNavs/DoctorsNavs'
+import Quality from '@/components/Quality/Quality'
 import Hero from '@/components/hero/Hero'
-import React from "react";
 
 function Doctor() {
-  return (
-    <div>
-      <Hero/>
-      <DoctorsNavs />
-      <DoctorInformation/>
-    </div>
-  );
+	return (
+		<div>
+			<Hero />
+			<DoctorsNavs />
+			<DoctorInformation />
+			<Quality />
+		</div>
+	)
 }
 
-export default Doctor;
+export default Doctor
